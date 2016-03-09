d1b1ffb0da97d3fb32b8f0d27dd92b38fb3badf6
Erikheath Thomas <erikheath.thomas@me.com>
2016-03-08 20:33:42 -0800

    Project Setup Commit.

---
+++ DataDriver/DataDriver/DataDriver.h
@@ -0,0 +1,19 @@
+//
+//  DataDriver.h
+//  DataDriver
+//
+//  Created by ERIKHEATH A THOMAS on 3/8/16.
+//  Copyright © 2016 Curated Cocoa LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DataDriver.
+FOUNDATION_EXPORT double DataDriverVersionNumber;
+
+//! Project version string for DataDriver.
+FOUNDATION_EXPORT const unsigned char DataDriverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataDriver/PublicHeader.h>
+
+
