111e1f669af5f03d7e2592b2747764b052ff89d5
Erikheath Thomas <erikheath.thomas@me.com>
2016-04-05 12:50:36 -0700

    Added testing for dependencies among spawned operations.

--- DataDriver/DataDriverTests/TransactionOperationTests.swift
+++ DataDriver/DataDriverTests/TransactionOperationTests.swift
@@ -55,6 +55,62 @@ class TransactionOperationTests: XCTestCase {
         let transaction = TransactionOperation(request: request, graphManager: dataLayerOne.persistentStoreCoordinator.operationGraphManager)
         XCTAssertNotNil(transaction.internalQueue)
         XCTAssert(transaction.internalQueue.operationCount == 6)
+
+        var requestOp: RemoteStoreRequestOperation? = nil
+        for testOp in transaction.internalQueue.operations {
+            guard testOp is RemoteStoreRequestOperation else { continue }
+            requestOp = testOp as? RemoteStoreRequestOperation
+            break
+        }
+        XCTAssertNotNil(requestOp)
+        XCTAssert(requestOp!.conditions.count == 1)
+        XCTAssertNotNil(requestOp!.conditions.first as? DataConditionerCondition)
+        XCTAssert(requestOp!.dependencies.count == 2)
+
+        var dataCondOp: DataConditionerOperation? = nil
+        for testOp in requestOp!.dependencies {
+            guard testOp is DataConditionerOperation else { continue }
+            dataCondOp = testOp as? DataConditionerOperation
+            break
+        }
+        XCTAssertNotNil(dataCondOp)
+        XCTAssert(dataCondOp!.conditions.count == 1)
+        XCTAssertNotNil(dataCondOp!.conditions.first as? RequestDataCondition)
+        XCTAssert(dataCondOp!.dependencies.count == 2)
+
+        var reqDataOp: RequestDataOperation? = nil
+        for testOp in dataCondOp!.dependencies {
+            guard testOp is RequestDataOperation else { continue }
+            reqDataOp = testOp as? RequestDataOperation
+            break
+        }
+        XCTAssertNotNil(reqDataOp)
+        XCTAssert(reqDataOp!.conditions.count == 1)
+        XCTAssertNotNil(reqDataOp!.conditions.first as? RequestConstructionCondition)
+        XCTAssert(dataCondOp!.dependencies.count == 2)
+
+        var reqConOp: RequestConstructionOperation? = nil
+        for testOp in reqDataOp!.dependencies {
+            guard testOp is RequestConstructionOperation else { continue }
+            reqConOp = testOp as? RequestConstructionOperation
+            break
+        }
+        XCTAssertNotNil(reqConOp)
+        XCTAssert(reqConOp!.conditions.count == 1)
+        XCTAssertNotNil(reqConOp!.conditions.first as? RequestValidationCondition)
+        XCTAssert(reqConOp!.dependencies.count == 2)
+
+        var reqValOp: RequestValidation? = nil
+        for testOp in reqConOp!.dependencies {
+            guard testOp is RequestValidation else { continue }
+            reqValOp = testOp as? RequestValidation
+            break
+        }
+        XCTAssertNotNil(reqValOp)
+        XCTAssert(reqValOp!.conditions.count == 0)
+        XCTAssertNil(reqValOp!.conditions.first)
+        XCTAssert(reqValOp!.dependencies.count == 1)
+
     }
 
     
